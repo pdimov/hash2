43cf05932e918051f187716bb4d77c0ad5b29ac8
Christian Mazakas <christian.mazakas@gmail.com>
2024-10-01 13:10:11 -0700

    add sha-512 implementation

--- include/boost/hash2/sha2.hpp
+++ include/boost/hash2/sha2.hpp
@@ -280,6 +280,220 @@ public:
     }
 };
 
+class sha2_512
+{
+private:
+
+    std::uint64_t state_[ 8 ];
+
+    static const int N = 128;
+
+    unsigned char buffer_[ N ];
+    std::size_t m_; // == n_ % N
+
+    std::uint64_t n_;
+
+private:
+
+    void init()
+    {
+        state_[ 0 ] = 0x6a09e667f3bcc908;
+        state_[ 1 ] = 0xbb67ae8584caa73b;
+        state_[ 2 ] = 0x3c6ef372fe94f82b;
+        state_[ 3 ] = 0xa54ff53a5f1d36f1;
+        state_[ 4 ] = 0x510e527fade682d1;
+        state_[ 5 ] = 0x9b05688c2b3e6c1f;
+        state_[ 6 ] = 0x1f83d9abfb41bd6b;
+        state_[ 7 ] = 0x5be0cd19137e2179;
+    }
+
+    static std::uint64_t Ch( std::uint64_t x, std::uint64_t y, std::uint64_t z ) noexcept
+    {
+        return ( x & y ) ^ ( ~x & z );
+    }
+
+    static std::uint64_t Maj( std::uint64_t x, std::uint64_t y, std::uint64_t z ) noexcept
+    {
+        return ( x & y ) ^ ( x & z ) ^ ( y & z );
+    }
+
+    static std::uint64_t Sigma0( std::uint64_t x ) noexcept
+    {
+        return detail::rotr( x, 28 ) ^ detail::rotr( x, 34 ) ^ detail::rotr( x, 39 );
+    }
+
+    static std::uint64_t Sigma1( std::uint64_t x ) noexcept
+    {
+        return detail::rotr( x, 14 ) ^ detail::rotr( x, 18 ) ^ detail::rotr( x, 41 );
+    }
+
+    static std::uint64_t sigma0( std::uint64_t x ) noexcept
+    {
+        return detail::rotr( x, 1 ) ^ detail::rotr( x, 8 ) ^ ( x >> 7 );
+    }
+
+    static std::uint64_t sigma1( std::uint64_t x ) noexcept
+    {
+        return detail::rotr( x, 19 ) ^ detail::rotr( x, 61 ) ^ ( x >> 6 );
+    }
+
+    void transform( unsigned char const block[ N ] )
+    {
+        constexpr static std::uint64_t const K[ 80 ] =
+        {
+            0x428a2f98d728ae22, 0x7137449123ef65cd, 0xb5c0fbcfec4d3b2f, 0xe9b5dba58189dbbc,
+            0x3956c25bf348b538, 0x59f111f1b605d019, 0x923f82a4af194f9b, 0xab1c5ed5da6d8118,
+            0xd807aa98a3030242, 0x12835b0145706fbe, 0x243185be4ee4b28c, 0x550c7dc3d5ffb4e2,
+            0x72be5d74f27b896f, 0x80deb1fe3b1696b1, 0x9bdc06a725c71235, 0xc19bf174cf692694,
+            0xe49b69c19ef14ad2, 0xefbe4786384f25e3, 0x0fc19dc68b8cd5b5, 0x240ca1cc77ac9c65,
+            0x2de92c6f592b0275, 0x4a7484aa6ea6e483, 0x5cb0a9dcbd41fbd4, 0x76f988da831153b5,
+            0x983e5152ee66dfab, 0xa831c66d2db43210, 0xb00327c898fb213f, 0xbf597fc7beef0ee4,
+            0xc6e00bf33da88fc2, 0xd5a79147930aa725, 0x06ca6351e003826f, 0x142929670a0e6e70,
+            0x27b70a8546d22ffc, 0x2e1b21385c26c926, 0x4d2c6dfc5ac42aed, 0x53380d139d95b3df,
+            0x650a73548baf63de, 0x766a0abb3c77b2a8, 0x81c2c92e47edaee6, 0x92722c851482353b,
+            0xa2bfe8a14cf10364, 0xa81a664bbc423001, 0xc24b8b70d0f89791, 0xc76c51a30654be30,
+            0xd192e819d6ef5218, 0xd69906245565a910, 0xf40e35855771202a, 0x106aa07032bbd1b8,
+            0x19a4c116b8d2d0c8, 0x1e376c085141ab53, 0x2748774cdf8eeb99, 0x34b0bcb5e19b48a8,
+            0x391c0cb3c5c95a63, 0x4ed8aa4ae3418acb, 0x5b9cca4f7763e373, 0x682e6ff3d6b2b8a3,
+            0x748f82ee5defb2fc, 0x78a5636f43172f60, 0x84c87814a1f0ab72, 0x8cc702081a6439ec,
+            0x90befffa23631e28, 0xa4506cebde82bde9, 0xbef9a3f7b2c67915, 0xc67178f2e372532b,
+            0xca273eceea26619c, 0xd186b8c721c0c207, 0xeada7dd6cde0eb1e, 0xf57d4f7fee6ed178,
+            0x06f067aa72176fba, 0x0a637dc5a2c898a6, 0x113f9804bef90dae, 0x1b710b35131c471b,
+            0x28db77f523047d84, 0x32caab7b40c72493, 0x3c9ebe0a15c9bebc, 0x431d67c49c100d4c,
+            0x4cc5d4becb3e42b6, 0x597f299cfc657e2a, 0x5fcb6fab3ad6faec, 0x6c44198c4a475817,
+        };
+
+        std::uint64_t W[ 80 ];
+
+        for( int t = 0; t < 16; ++t )
+        {
+            W[ t ] = detail::read64be( block + t * 8 );
+        }
+
+        for( int t = 16; t < 80; ++t )
+        {
+            W[ t ] = sigma1( W[ t - 2 ] ) + W[ t - 7 ] + sigma0( W[ t - 15 ] ) + W[ t - 16 ];
+        }
+
+        std::uint64_t a = state_[ 0 ];
+        std::uint64_t b = state_[ 1 ];
+        std::uint64_t c = state_[ 2 ];
+        std::uint64_t d = state_[ 3 ];
+        std::uint64_t e = state_[ 4 ];
+        std::uint64_t f = state_[ 5 ];
+        std::uint64_t g = state_[ 6 ];
+        std::uint64_t h = state_[ 7 ];
+
+        for( int t = 0; t < 80; ++t )
+        {
+            std::uint64_t T1 = h + Sigma1( e ) + Ch( e, f, g ) + K[ t ] + W[ t ];
+            std::uint64_t T2 = Sigma0( a ) + Maj( a, b, c );
+
+            h = g;
+            g = f;
+            f = e;
+            e = d + T1;
+            d = c;
+            c = b;
+            b = a;
+            a = T1 + T2;
+        }
+
+        state_[ 0 ] += a;
+        state_[ 1 ] += b;
+        state_[ 2 ] += c;
+        state_[ 3 ] += d;
+        state_[ 4 ] += e;
+        state_[ 5 ] += f;
+        state_[ 6 ] += g;
+        state_[ 7 ] += h;
+    }
+
+public:
+
+    using result_type = std::array<unsigned char, 64>;
+
+    sha2_512(): m_( 0 ), n_( 0 )
+    {
+        init();
+    }
+
+    void update( void const * pv, std::size_t n )
+    {
+        unsigned char const* p = static_cast<unsigned char const*>( pv );
+
+        BOOST_ASSERT( m_ == n_ % N );
+
+        n_ += n;
+
+        if( m_ > 0 )
+        {
+            std::size_t k = N - m_;
+
+            if( n < k )
+            {
+                k = n;
+            }
+
+            std::memcpy( buffer_ + m_, p, k );
+
+            p += k;
+            n -= k;
+            m_ += k;
+
+            if( m_ < N ) return;
+
+            BOOST_ASSERT( m_ == N );
+
+            transform( buffer_ );
+            m_ = 0;
+
+            std::memset( buffer_, 0, N );
+        }
+
+        BOOST_ASSERT( m_ == 0 );
+
+        while( n >= N )
+        {
+            transform( p );
+
+            p += N;
+            n -= N;
+        }
+
+        BOOST_ASSERT( n < N );
+
+        if( n > 0 )
+        {
+            std::memcpy( buffer_, p, n );
+            m_ = n;
+        }
+
+        BOOST_ASSERT( m_ == n_ % N );
+    }
+
+    result_type result()
+    {
+        unsigned char bits[ 16 ] = {};
+        detail::write64be( bits + 8, n_ * 8 );
+
+        std::size_t k = m_ < (N - 16) ? (N - 16) - m_ : N + ( N - 16 ) - m_;
+        unsigned char padding[ N ] = { 0x80 };
+
+        update( padding, k );
+        update( bits, 16 );
+        BOOST_ASSERT( m_ == 0 );
+
+        result_type digest;
+        for( int i = 0; i < 8; ++i )
+        {
+            detail::write64be( &digest[ i * 8 ], state_[ i ] );
+        }
+
+        return digest;
+    }
+};
+
 } // namespace hash2
 } // namespace boost
 
