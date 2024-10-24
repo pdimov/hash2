6a4f161c9846875d658201c10debb01af5baa7c4
Peter Dimov <pdimov@gmail.com>
2024-10-24 22:34:04 +0300

    Add optimized `reverse` overloads using MSVC bswap intrinsics

--- include/boost/hash2/detail/reverse.hpp
+++ include/boost/hash2/detail/reverse.hpp
@@ -60,6 +60,41 @@ inline void reverse( unsigned char (&d)[ 16 ], void const* s )
     std::memcpy( d, tmp2, 16 );
 }
 
+#elif defined(_MSC_VER)
+
+inline void reverse( unsigned char (&d)[ 2 ], void const* s )
+{
+    unsigned short tmp;
+    std::memcpy( &tmp, s, 2 );
+    tmp = _byteswap_ushort( tmp );
+    std::memcpy( d, &tmp, 2 );
+}
+
+inline void reverse( unsigned char (&d)[ 4 ], void const* s )
+{
+    unsigned long tmp;
+    std::memcpy( &tmp, s, 4 );
+    tmp = _byteswap_ulong( tmp );
+    std::memcpy( d, &tmp, 4 );
+}
+
+inline void reverse( unsigned char (&d)[ 8 ], void const* s )
+{
+    unsigned long long tmp;
+    std::memcpy( &tmp, s, 8 );
+    tmp = _byteswap_uint64( tmp );
+    std::memcpy( d, &tmp, 8 );
+}
+
+inline void reverse( unsigned char (&d)[ 16 ], void const* s )
+{
+    unsigned long long tmp[ 2 ];
+    std::memcpy( tmp, s, 16 );
+
+    unsigned long long tmp2[ 2 ] = { _byteswap_uint64( tmp[1] ), _byteswap_uint64( tmp[0] ) };
+    std::memcpy( d, tmp2, 16 );
+}
+
 #endif
 
 } // namespace detail
