1bb59f72053bb819de11f4722118d040a5db9bb8
Peter Dimov <pdimov@gmail.com>
2024-10-24 16:47:31 +0300

    Change H to Hash in hash_append.hpp

--- include/boost/hash2/hash_append.hpp
+++ include/boost/hash2/hash_append.hpp
@@ -32,18 +32,18 @@ namespace hash2
 
 // forward declarations
 
-template<class H, class T> void hash_append( H & h, T const & v );
-template<class H, class It> void hash_append_range( H & h, It first, It last );
-template<class H, class T> void hash_append_size( H & h, T const & v );
-template<class H, class It> void hash_append_sized_range( H & h, It first, It last );
-template<class H, class It> void hash_append_unordered_range( H & h, It first, It last );
+template<class Hash, class T> void hash_append( Hash& h, T const& v );
+template<class Hash, class It> void hash_append_range( Hash& h, It first, It last );
+template<class Hash, class T> void hash_append_size( Hash& h, T const& v );
+template<class Hash, class It> void hash_append_sized_range( Hash& h, It first, It last );
+template<class Hash, class It> void hash_append_unordered_range( Hash& h, It first, It last );
 
 // hash_append_range
 
 namespace detail
 {
 
-template<class H, class It> void hash_append_range_( H & h, It first, It last )
+template<class Hash, class It> void hash_append_range_( Hash& h, It first, It last )
 {
     for( ; first != last; ++first )
     {
@@ -51,36 +51,36 @@ template<class H, class It> void hash_append_range_( H & h, It first, It last )
     }
 }
 
-template<class H> void hash_append_range_( H & h, unsigned char * first, unsigned char * last )
+template<class Hash> void hash_append_range_( Hash& h, unsigned char* first, unsigned char* last )
 {
     h.update( first, last - first );
 }
 
-template<class H> void hash_append_range_( H & h, unsigned char const * first, unsigned char const * last )
+template<class Hash> void hash_append_range_( Hash& h, unsigned char const* first, unsigned char const* last )
 {
     h.update( first, last - first );
 }
 
-template<class H, class T>
+template<class Hash, class T>
     typename std::enable_if<
         is_contiguously_hashable<T, endian::native>::value, void >::type
-    hash_append_range_( H & h, T * first, T * last )
+    hash_append_range_( Hash& h, T* first, T* last )
 {
     h.update( first, (last - first) * sizeof(T) );
 }
 
 } // namespace detail
 
-template<class H, class It> void hash_append_range( H & h, It first, It last )
+template<class Hash, class It> void hash_append_range( Hash& h, It first, It last )
 {
     detail::hash_append_range_( h, first, last );
 }
 
 // hash_append_size
 
-template<class H, class T> void hash_append_size( H & h, T const & v )
+template<class Hash, class T> void hash_append_size( Hash& h, T const& v )
 {
-    hash_append( h, static_cast<typename H::size_type>( v ) );
+    hash_append( h, static_cast<typename Hash::size_type>( v ) );
 }
 
 // hash_append_sized_range
@@ -88,7 +88,7 @@ template<class H, class T> void hash_append_size( H & h, T const & v )
 namespace detail
 {
 
-template<class H, class It> void hash_append_sized_range_( H & h, It first, It last, std::input_iterator_tag )
+template<class Hash, class It> void hash_append_sized_range_( Hash& h, It first, It last, std::input_iterator_tag )
 {
     typename std::iterator_traits<It>::difference_type m = 0;
 
@@ -100,7 +100,7 @@ template<class H, class It> void hash_append_sized_range_( H & h, It first, It l
     hash_append_size( h, m );
 }
 
-template<class H, class It> void hash_append_sized_range_( H & h, It first, It last, std::random_access_iterator_tag )
+template<class Hash, class It> void hash_append_sized_range_( Hash& h, It first, It last, std::random_access_iterator_tag )
 {
     hash_append_range( h, first, last );
     hash_append_size( h, last - first );
@@ -108,14 +108,14 @@ template<class H, class It> void hash_append_sized_range_( H & h, It first, It l
 
 } // namespace detail
 
-template<class H, class It> void hash_append_sized_range( H & h, It first, It last )
+template<class Hash, class It> void hash_append_sized_range( Hash& h, It first, It last )
 {
     detail::hash_append_sized_range_( h, first, last, typename std::iterator_traits<It>::iterator_category() );
 }
 
 // hash_append_unordered_range
 
-template<class H, class It> void hash_append_unordered_range( H & h, It first, It last )
+template<class Hash, class It> void hash_append_unordered_range( Hash& h, It first, It last )
 {
     typename std::iterator_traits<It>::difference_type m = 0;
 
@@ -123,8 +123,7 @@ template<class H, class It> void hash_append_unordered_range( H & h, It first, I
 
     for( ; first != last; ++first, ++m )
     {
-        H h2( h );
-
+        Hash h2( h );
         hash_append( h2, *first );
 
         w += get_integral_result<std::uint64_t>( h2.result() );
@@ -138,10 +137,10 @@ template<class H, class It> void hash_append_unordered_range( H & h, It first, I
 
 // contiguously hashable (this includes unsigned char const&)
 
-template<class H, class T>
+template<class Hash, class T>
     typename std::enable_if<
         is_contiguously_hashable<T, endian::native>::value, void >::type
-    do_hash_append( H & h, T const & v )
+    do_hash_append( Hash& h, T const& v )
 {
     unsigned char const * p = reinterpret_cast<unsigned char const*>( &v );
     hash_append_range( h, p, p + sizeof(T) );
@@ -149,9 +148,9 @@ template<class H, class T>
 
 // floating point
 
-template<class H, class T>
+template<class Hash, class T>
     typename std::enable_if< std::is_floating_point<T>::value, void >::type
-    do_hash_append( H & h, T const & v )
+    do_hash_append( Hash& h, T const& v )
 {
     T w = v == 0? 0: v;
 
@@ -161,16 +160,16 @@ template<class H, class T>
 
 // C arrays
 
-template<class H, class T, std::size_t N> void do_hash_append( H & h, T const (&v)[ N ] )
+template<class Hash, class T, std::size_t N> void do_hash_append( Hash& h, T const (&v)[ N ] )
 {
     hash_append_range( h, v + 0, v + N );
 }
 
 // contiguous containers and ranges, w/ size
 
-template<class H, class T>
+template<class Hash, class T>
     typename std::enable_if< container_hash::is_contiguous_range<T>::value && !container_hash::is_tuple_like<T>::value, void >::type
-    do_hash_append( H & h, T const & v )
+    do_hash_append( Hash& h, T const& v )
 {
     hash_append_range( h, v.data(), v.data() + v.size() );
     hash_append_size( h, v.size() );
@@ -178,34 +177,34 @@ template<class H, class T>
 
 // containers and ranges, w/ size
 
-template<class H, class T>
+template<class Hash, class T>
     typename std::enable_if< container_hash::is_range<T>::value && !container_hash::is_tuple_like<T>::value && !container_hash::is_contiguous_range<T>::value && !container_hash::is_unordered_range<T>::value, void >::type
-    do_hash_append( H & h, T const & v )
+    do_hash_append( Hash& h, T const& v )
 {
     hash_append_sized_range( h, v.begin(), v.end() );
 }
 
 // std::array (both range and tuple-like)
 
-template<class H, class T>
+template<class Hash, class T>
     typename std::enable_if< container_hash::is_range<T>::value && container_hash::is_tuple_like<T>::value, void >::type
-    do_hash_append( H & h, T const & v )
+    do_hash_append( Hash& h, T const& v )
 {
     hash_append_range( h, v.begin(), v.end() );
 }
 
 // boost::array (constant size, but not tuple-like)
 
-template<class H, class T, std::size_t N> void do_hash_append( H & h, boost::array<T, N> const & v )
+template<class Hash, class T, std::size_t N> void do_hash_append( Hash& h, boost::array<T, N> const& v )
 {
     hash_append_range( h, v.begin(), v.end() );
 }
 
 // unordered containers (is_unordered_range implies is_range)
 
-template<class H, class T>
+template<class Hash, class T>
     typename std::enable_if< container_hash::is_unordered_range<T>::value, void >::type
-    do_hash_append( H & h, T const & v )
+    do_hash_append( Hash& h, T const& v )
 {
     hash_append_unordered_range( h, v.begin(), v.end() );
 }
@@ -215,7 +214,7 @@ template<class H, class T>
 namespace detail
 {
 
-template<class H, class T, std::size_t... J> void hash_append_tuple( H & h, T const& v, boost::mp11::integer_sequence<std::size_t, J...> )
+template<class Hash, class T, std::size_t... J> void hash_append_tuple( Hash& h, T const& v, boost::mp11::integer_sequence<std::size_t, J...> )
 {
     using std::get;
     int a[] = { 0, ((void)hash_append( h, get<J>(v) ), 0)... };
@@ -224,9 +223,9 @@ template<class H, class T, std::size_t... J> void hash_append_tuple( H & h, T co
 
 } // namespace detail
 
-template<class H, class T>
+template<class Hash, class T>
     typename std::enable_if< !container_hash::is_range<T>::value && container_hash::is_tuple_like<T>::value, void >::type
-    do_hash_append( H & h, T const & v )
+    do_hash_append( Hash& h, T const& v )
 {
     typedef boost::mp11::make_index_sequence<std::tuple_size<T>::value> seq;
     detail::hash_append_tuple( h, v, seq() );
@@ -234,7 +233,7 @@ template<class H, class T>
 
 // hash_append
 
-template<class H, class T> void hash_append( H & h, T const & v )
+template<class Hash, class T> void hash_append( Hash& h, T const& v )
 {
     do_hash_append( h, v );
 }
