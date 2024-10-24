e319dcbcfc9dc47d2f4f1180ae8f10dd6c99e135
Peter Dimov <pdimov@gmail.com>
2024-10-24 17:43:55 +0300

    Update test/hash_append_3.cpp

--- test/hash_append_3.cpp
+++ test/hash_append_3.cpp
@@ -13,32 +13,35 @@
 #include <set>
 #include <forward_list>
 
-template<class H, class T, class R> void test_sized_range( R r )
+template<class Hash, class Flavor, class T, class R> void test_sized_range( R r )
 {
     int w[] = { 1, 2, 3, 4 };
     T v( w, w + sizeof(w) / sizeof(w[0]) );
 
-    H h;
+    Hash h;
+    Flavor f;
 
-    hash_append( h, v );
+    hash_append( h, f, v );
 
     BOOST_TEST_EQ( h.result(), r );
 }
 
-template<class H, class R> void test( R r )
+template<class Hash, class Flavor, class R> void test( R r )
 {
-    test_sized_range< H, std::vector<int> >( r );
-    test_sized_range< H, std::list<int> >( r );
-    test_sized_range< H, std::deque<int> >( r );
-    test_sized_range< H, std::set<int> >( r );
-    test_sized_range< H, std::multiset<int> >( r );
-    test_sized_range< H, std::forward_list<int> >( r );
+    test_sized_range< Hash, Flavor, std::vector<int> >( r );
+    test_sized_range< Hash, Flavor, std::list<int> >( r );
+    test_sized_range< Hash, Flavor, std::deque<int> >( r );
+    test_sized_range< Hash, Flavor, std::set<int> >( r );
+    test_sized_range< Hash, Flavor, std::multiset<int> >( r );
+    test_sized_range< Hash, Flavor, std::forward_list<int> >( r );
 }
 
 int main()
 {
-    test<boost::hash2::fnv1a_32>( 1745310485ul );
-    test<boost::hash2::fnv1a_64>( 8602726560470947781ull );
+    using namespace boost::hash2;
+
+    test<fnv1a_32, default_flavor>( 1745310485ul );
+    test<fnv1a_64, default_flavor>( 13335575246003589621ull );
 
     return boost::report_errors();
 }
