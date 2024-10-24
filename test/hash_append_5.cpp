7043960f3852f7b87856224a553475a7bf0055f1
Peter Dimov <pdimov@gmail.com>
2024-10-24 17:51:31 +0300

    Update test/hash_append_5.cpp

--- test/hash_append_5.cpp
+++ test/hash_append_5.cpp
@@ -15,7 +15,7 @@ namespace boost
 namespace hash2
 {
 
-template<class H, class T> void hash_append( H & h, T const & v );
+template<class Hash, class Flavor, class T> void hash_append( Hash& h, Flavor const& f, T const& v );
 
 } // namespace hash2
 } // namespace boost
@@ -32,12 +32,10 @@ private:
     std::string s1_;
     std::string s2_;
 
-    template<class H> friend void do_hash_append( H& h, X const& x )
+    template<class Hash, class Flavor> friend void do_hash_append( Hash& h, Flavor const& f, X const& x )
     {
-        using boost::hash2::hash_append;
-
-        hash_append( h, x.s1_ );
-        hash_append( h, x.s2_ );
+        boost::hash2::hash_append( h, f, x.s1_ );
+        boost::hash2::hash_append( h, f, x.s2_ );
     }
 
 public:
@@ -66,13 +64,12 @@ private:
 public:
 
     typedef std::uint32_t result_type;
-    typedef std::uint32_t size_type;
 
     fnv1a(): st_( 0x811C9DC5ul )
     {
     }
 
-    void update( void const * pv, std::size_t n )
+    void update( void const* pv, std::size_t n )
     {
         unsigned char const* p = static_cast<unsigned char const*>( pv );
 
@@ -107,24 +104,22 @@ public:
 #include <vector>
 #include <list>
 
-template<class H, class R> void test( R r )
+template<class Hash, class Flavor, class R> void test( R r )
 {
     {
-        H h;
+        Hash h;
+        Flavor f;
 
-        using boost::hash2::hash_append;
-
-        hash_append( h, std::vector<lib1::X>( 3 ) );
+        boost::hash2::hash_append( h, f, std::vector<lib1::X>( 3 ) );
 
         BOOST_TEST_EQ( h.result(), r );
     }
 
     {
-        H h;
+        Hash h;
+        Flavor f;
 
-        using boost::hash2::hash_append;
-
-        hash_append( h, std::list<lib1::X>( 3 ) );
+        boost::hash2::hash_append( h, f, std::list<lib1::X>( 3 ) );
 
         BOOST_TEST_EQ( h.result(), r );
     }
@@ -132,8 +127,8 @@ template<class H, class R> void test( R r )
 
 int main()
 {
-    test<boost::hash2::fnv1a_32>( 2425039999ul );
-    test<lib2::fnv1a>( 2425039999ul );
+    test<boost::hash2::fnv1a_32, boost::hash2::default_flavor>( 2425039999ul );
+    test<lib2::fnv1a, boost::hash2::default_flavor>( 2425039999ul );
 
     return boost::report_errors();
 }
