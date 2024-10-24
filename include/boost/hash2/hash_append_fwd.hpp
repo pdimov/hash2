6e20fb96de420595bbf90997e4498aaffbe62650
Peter Dimov <pdimov@gmail.com>
2024-10-24 20:26:02 +0300

    Work around GCC 11 and earlier inability to handle default template arguments correctly

--- include/boost/hash2/hash_append_fwd.hpp
+++ include/boost/hash2/hash_append_fwd.hpp
@@ -5,16 +5,35 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
+#include <boost/config.hpp>
+
 namespace boost
 {
 namespace hash2
 {
+
+#if defined(BOOST_GCC) && BOOST_GCC < 120000
+
+// Due to a bug in GCC 11 and earlier, the default argument
+// for Flavor needs to be present on the first declaration
+
+struct default_flavor;
+
+template<class Hash, class Flavor = default_flavor, class T> void hash_append( Hash& h, Flavor const& f, T const& v );
+template<class Hash, class Flavor = default_flavor, class It> void hash_append_range( Hash& h, Flavor const& f, It first, It last );
+template<class Hash, class Flavor = default_flavor, class T> void hash_append_size( Hash& h, Flavor const& f, T const& v );
+template<class Hash, class Flavor = default_flavor, class It> void hash_append_sized_range( Hash& h, Flavor const& f, It first, It last );
+template<class Hash, class Flavor = default_flavor, class It> void hash_append_unordered_range( Hash& h, Flavor const& f, It first, It last );
+
+#else
 
 template<class Hash, class Flavor, class T> void hash_append( Hash& h, Flavor const& f, T const& v );
 template<class Hash, class Flavor, class It> void hash_append_range( Hash& h, Flavor const& f, It first, It last );
 template<class Hash, class Flavor, class T> void hash_append_size( Hash& h, Flavor const& f, T const& v );
 template<class Hash, class Flavor, class It> void hash_append_sized_range( Hash& h, Flavor const& f, It first, It last );
 template<class Hash, class Flavor, class It> void hash_append_unordered_range( Hash& h, Flavor const& f, It first, It last );
+
+#endif
 
 } // namespace hash2
 } // namespace boost
