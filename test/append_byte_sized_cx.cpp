5a81d36da5c84c9a8f400a9a7a9150c17f78820c
Peter Dimov <pdimov@gmail.com>
2024-11-03 20:21:27 +0200

    Update test/append_byte_sized_cx.cpp

--- test/append_byte_sized_cx.cpp
+++ test/append_byte_sized_cx.cpp
@@ -4,26 +4,13 @@
 
 #include <boost/hash2/hash_append.hpp>
 #include <boost/hash2/fnv1a.hpp>
+#include <boost/core/lightweight_test.hpp>
 #include <boost/config.hpp>
-#include <boost/config/pragma_message.hpp>
-
-#if defined(BOOST_NO_CXX14_CONSTEXPR)
-
-BOOST_PRAGMA_MESSAGE( "Test skipped, because BOOST_NO_CXX14_CONSTEXPR is defined" )
-int main() {}
-
-#elif defined(BOOST_GCC) && BOOST_GCC < 60000
-
-BOOST_PRAGMA_MESSAGE( "Test skipped, because BOOST_GCC is < 60000" )
-int main() {}
-
-#else
+#include <cstdint>
 
 #if defined(BOOST_MSVC) && BOOST_MSVC < 1920
 # pragma warning(disable: 4307) // integral constant overflow
 #endif
-
-#define STATIC_ASSERT(...) static_assert(__VA_ARGS__, #__VA_ARGS__)
 
 using namespace boost::hash2;
 
@@ -37,25 +24,35 @@ template<class T> BOOST_CXX14_CONSTEXPR std::uint32_t test()
     return h.result();
 }
 
+#define STATIC_ASSERT(...) static_assert(__VA_ARGS__, #__VA_ARGS__)
+
+#if defined(BOOST_NO_CXX14_CONSTEXPR) || ( defined(BOOST_GCC) && BOOST_GCC < 60000 )
+
+# define TEST_EQ(x1, x2) BOOST_TEST_EQ(x1, x2)
+
+#else
+
+# define TEST_EQ(x1, x2) BOOST_TEST_EQ(x1, x2); STATIC_ASSERT(x1 == x2)
+
+#endif
+
 enum E: unsigned char {};
 
 int main()
 {
     constexpr std::uint32_t r = 409807047;
 
-    STATIC_ASSERT( test<bool>() == r );
-    STATIC_ASSERT( test<char>() == r );
-    STATIC_ASSERT( test<signed char>() == r );
-    STATIC_ASSERT( test<unsigned char>() == r );
-    STATIC_ASSERT( test<E>() == r );
+    TEST_EQ( test<bool>(), r );
+    TEST_EQ( test<char>(), r );
+    TEST_EQ( test<signed char>(), r );
+    TEST_EQ( test<unsigned char>(), r );
+    TEST_EQ( test<E>(), r );
 
 #if defined(__cpp_lib_byte) && __cpp_lib_byte >= 201603L
-    STATIC_ASSERT( test<std::byte>() == r );
+    TEST_EQ( test<std::byte>(), r );
 #endif
 
 #if defined(__cpp_char8_t) && __cpp_char8_t >= 201811L
-    STATIC_ASSERT( test<char8_t>() == r );
+    TEST_EQ( test<char8_t>(), r );
 #endif
 }
-
-#endif
