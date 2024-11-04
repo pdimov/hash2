a633d33c489beaf4fb1448e1bb5a4670581c288a
Peter Dimov <pdimov@gmail.com>
2024-11-04 17:36:18 +0200

    Update test/is_contiguous_range.cpp

--- test/is_contiguous_range.cpp
+++ test/is_contiguous_range.cpp
@@ -1,9 +1,13 @@
-// Copyright 2017, 2018 Peter Dimov.
+// Copyright 2017, 2018, 2024 Peter Dimov
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
+#include <boost/hash2/digest.hpp>
 #include <boost/container_hash/is_contiguous_range.hpp>
 #include <boost/core/lightweight_test_trait.hpp>
+#include <boost/array.hpp>
+#include <boost/config.hpp>
+#include <boost/config/workaround.hpp>
 #include <string>
 #include <vector>
 #include <list>
@@ -62,6 +66,9 @@ int main()
     BOOST_TEST_TRAIT_TRUE((is_contiguous_range< std::array<X, 2> >));
     BOOST_TEST_TRAIT_TRUE((is_contiguous_range< std::array<X, 2> const >));
 
+    BOOST_TEST_TRAIT_TRUE((is_contiguous_range< std::array<X, 0> >));
+    BOOST_TEST_TRAIT_TRUE((is_contiguous_range< std::array<X, 0> const >));
+
     BOOST_TEST_TRAIT_FALSE((is_contiguous_range< std::forward_list<X> >));
     BOOST_TEST_TRAIT_FALSE((is_contiguous_range< std::forward_list<X> const >));
 
@@ -76,6 +83,19 @@ int main()
 
     BOOST_TEST_TRAIT_FALSE((is_contiguous_range< std::unordered_multimap<int, X> >));
     BOOST_TEST_TRAIT_FALSE((is_contiguous_range< std::unordered_multimap<int, X> const >));
+
+#if !BOOST_WORKAROUND(BOOST_MSVC, < 1910)
+
+    BOOST_TEST_TRAIT_TRUE((is_contiguous_range< boost::array<X, 2> >));
+    BOOST_TEST_TRAIT_TRUE((is_contiguous_range< boost::array<X, 2> const >));
+
+    BOOST_TEST_TRAIT_TRUE((is_contiguous_range< boost::array<X, 0> >));
+    BOOST_TEST_TRAIT_TRUE((is_contiguous_range< boost::array<X, 0> const >));
+
+    BOOST_TEST_TRAIT_TRUE((is_contiguous_range< boost::hash2::digest<7> >));
+    BOOST_TEST_TRAIT_TRUE((is_contiguous_range< boost::hash2::digest<7> const >));
+
+#endif
 
     return boost::report_errors();
 }
