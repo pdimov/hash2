5724c5a714b1ea12dae379511b1596898002de6d
Peter Dimov <pdimov@gmail.com>
2024-11-06 02:47:12 +0200

    Update test/spihash_cx.cpp

--- test/siphash_cx.cpp
+++ test/siphash_cx.cpp
@@ -12,17 +12,27 @@
 # pragma warning(disable: 4307) // integral constant overflow
 #endif
 
-#define STATIC_ASSERT(...) static_assert(__VA_ARGS__, #__VA_ARGS__)
-
 template<class H, std::size_t N> BOOST_CXX14_CONSTEXPR typename H::result_type test( unsigned char const (&seed)[ 16 ], unsigned char const (&v)[ N ] )
 {
     H h( seed, 16 );
 
     h.update( v, N / 3 );
-    h.update( v, N - N / 3 );
+    h.update( v + N / 3, N - N / 3 );
 
     return h.result();
 }
+
+#define STATIC_ASSERT(...) static_assert(__VA_ARGS__, #__VA_ARGS__)
+
+#if defined(BOOST_NO_CXX14_CONSTEXPR)
+
+# define TEST_EQ(x1, x2) BOOST_TEST_EQ(x1, x2)
+
+#else
+
+# define TEST_EQ(x1, x2) BOOST_TEST_EQ(x1, x2); STATIC_ASSERT(x1 == x2)
+
+#endif
 
 int main()
 {
@@ -33,21 +43,11 @@ int main()
     constexpr unsigned char v21[ 21 ] = {};
     constexpr unsigned char v45[ 45 ] = {};
 
-    BOOST_TEST_EQ( test<siphash_32>( seed, v21 ), 3273912247 );
-    BOOST_TEST_EQ( test<siphash_32>( seed, v45 ), 3389005632 );
+    TEST_EQ( test<siphash_32>( seed, v21 ), 3273912247 );
+    TEST_EQ( test<siphash_32>( seed, v45 ), 3389005632 );
 
-    BOOST_TEST_EQ( test<siphash_64>( seed, v21 ), 17634937937087799533ull );
-    BOOST_TEST_EQ( test<siphash_64>( seed, v45 ), 7083435387605517692 );
-
-#if !defined(BOOST_NO_CXX14_CONSTEXPR)
-
-    STATIC_ASSERT( test<siphash_32>( seed, v21 ) == 3273912247 );
-    STATIC_ASSERT( test<siphash_32>( seed, v45 ) == 3389005632 );
-
-    STATIC_ASSERT( test<siphash_64>( seed, v21 ) == 17634937937087799533ull );
-    STATIC_ASSERT( test<siphash_64>( seed, v45 ) == 7083435387605517692 );
-
-#endif
+    TEST_EQ( test<siphash_64>( seed, v21 ), 17634937937087799533ull );
+    TEST_EQ( test<siphash_64>( seed, v45 ), 7083435387605517692 );
 
     return boost::report_errors();
 }
